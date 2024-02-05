acc3292c969296e49328a5e526acba9e3f1c81cb
luca savoretti <luca.savoretti@4ts.it>
2024-02-05 16:32:05 +0100

    aggiunto easy phone number

---
+++ easy--phone-number/PhoneNumber.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class PhoneNumber
+{
+    // https://learn.microsoft.com/it-it/dotnet/api/system.string?view=net-8.0  ---> metodi
+
+    public static string Clean(string phoneNumber)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
