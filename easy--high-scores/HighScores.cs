62e202fe1aeb57cd91177ec981d5b452bf074f1a
Lorenzo Tagliani <tagliani46@gmail.com>
2024-02-06 14:45:04 +0100

    Added functions to highscore

--- easy--high-scores/HighScores.cs
+++ easy--high-scores/HighScores.cs
@@ -1,103 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 public class HighScores
 {
-    private static List<int> _listaPunteggi {get;set;}
-
+    public List<int> lista {get; set;}
     // https://learn.microsoft.com/en-us/dotnet/csharp/linq/
     public HighScores(List<int> list)
     {
-        // salvo la lista nella variabile locale
-        _listaPunteggi = list;
-        // throw new NotImplementedException();
+        this.lista = list;
     }
 
     public List<int> Scores()
     {
-        // ritorno la lista dei punteggi
-        return _listaPunteggi;
-        // throw new NotImplementedException();
+        return lista;
     }
 
     public int Latest()
     {
-        // ritorno l'ultimo
-        int ultimo = -1;
-        foreach(var p in _listaPunteggi){
-            ultimo = p;
-        }
-
-        return ultimo;
-        // throw new NotImplementedException();
+        return lista[lista.Count - 1];
     }
 
     public int PersonalBest()
     {
-        // ritorno il migliore 
-
-        int best = -1;
-        foreach(var p in _listaPunteggi){
-            if(best < p){
-                best = p;
+        for (int i = 0; i < lista.Count - 1; i++)
+        {
+            for (int j = 0; j < lista.Count - i - 1; j++)
+            {
+                if (lista[j] > lista[j + 1])
+                {
+                    int temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
             }
         }
-
-        return best;
-
-        // throw new NotImplementedException();
+        return lista[lista.Count - 1];
     }
 
     public List<int> PersonalTopThree()
     {
-        // ritorno i migliori 3
-
-        var elementiDaRecuperare = 3;
-        var miglioritre = new List<int>();
-
-        // Sol1
-        // int processati = 0;
-        // foreach(var p in _listaPunteggi.OrderByDescending(p=> p)){
-        //     if(processati < elementiDaRecuperare){
-        //         miglioritre.Add(p);
-
-        //         processati++;
-        //     }
-        // }
-
-        // return miglioritre;
-
-        // Sol 2
-        // int[] array = _listaPunteggi.ToArray();
-        // int temporary;
-        // for (int j = 0; j <= array.Length - 2; j++)
-        // {
-        //     for (int i = 0; i <= array.Length - 2; i++)
-        //     {
-        //         if (array[i] < array[i + 1])
-        //         {
-        //             temporary= array[i + 1];
-        //             array[i + 1] = array[i];
-        //             array[i] = temporary;
-        //         }
-        //     }
-        // }
-
-        // int processati = 0;
-        // foreach(var p in array){
-        //     if(processati < elementiDaRecuperare){
-        //         miglioritre.Add(p);
-
-        //         processati++;
-        //     }
-        // }
-
-        // return miglioritre;
-
-         // Sol 3
-         return _listaPunteggi.OrderByDescending(p=> p).Take(3).ToList();
-
-        // throw new NotImplementedException();
+        /*
+        for (int i = 0; i < lista.Count - 1; i++)
+        {
+            for (int j = 0; j < lista.Count - i - 1; j++)
+            {
+                if (lista[j] > lista[j + 1])
+                {
+                    int temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
+            }
+        }
+        List<int> topthree = new List<int> {lista[lista.Count - 1], lista[lista.Count - 2], lista[lista.Count - 3],};
+        return topthree;
+        */
+        return lista.OrderByDescending(p=> p).Take(3).ToList();
     }
 }
