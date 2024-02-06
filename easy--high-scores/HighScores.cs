b8024aa4b36bdcf524d01a8c0700c8e4c86b2b19
luca savoretti <luca.savoretti@4ts.it>
2024-02-06 14:07:44 +0100

    soluzione commentata in classe dell'esercizio high-scores

--- easy--high-scores/HighScores.cs
+++ easy--high-scores/HighScores.cs
@@ -4,29 +4,100 @@ using System.Linq;
 
 public class HighScores
 {
+    private static List<int> _listaPunteggi {get;set;}
+
     // https://learn.microsoft.com/en-us/dotnet/csharp/linq/
     public HighScores(List<int> list)
     {
-        throw new NotImplementedException();
+        // salvo la lista nella variabile locale
+        _listaPunteggi = list;
+        // throw new NotImplementedException();
     }
 
     public List<int> Scores()
     {
-        throw new NotImplementedException();
+        // ritorno la lista dei punteggi
+        return _listaPunteggi;
+        // throw new NotImplementedException();
     }
 
     public int Latest()
     {
-        throw new NotImplementedException();
+        // ritorno l'ultimo
+        int ultimo = -1;
+        foreach(var p in _listaPunteggi){
+            ultimo = p;
+        }
+
+        return ultimo;
+        // throw new NotImplementedException();
     }
 
     public int PersonalBest()
     {
-        throw new NotImplementedException();
+        // ritorno il migliore 
+
+        int best = -1;
+        foreach(var p in _listaPunteggi){
+            if(best < p){
+                best = p;
+            }
+        }
+
+        return best;
+
+        // throw new NotImplementedException();
     }
 
     public List<int> PersonalTopThree()
     {
-        throw new NotImplementedException();
+        // ritorno i migliori 3
+
+        var elementiDaRecuperare = 3;
+        var miglioritre = new List<int>();
+
+        // Sol1
+        // int processati = 0;
+        // foreach(var p in _listaPunteggi.OrderByDescending(p=> p)){
+        //     if(processati < elementiDaRecuperare){
+        //         miglioritre.Add(p);
+
+        //         processati++;
+        //     }
+        // }
+
+        // return miglioritre;
+
+        // Sol 2
+        // int[] array = _listaPunteggi.ToArray();
+        // int temporary;
+        // for (int j = 0; j <= array.Length - 2; j++)
+        // {
+        //     for (int i = 0; i <= array.Length - 2; i++)
+        //     {
+        //         if (array[i] < array[i + 1])
+        //         {
+        //             temporary= array[i + 1];
+        //             array[i + 1] = array[i];
+        //             array[i] = temporary;
+        //         }
+        //     }
+        // }
+
+        // int processati = 0;
+        // foreach(var p in array){
+        //     if(processati < elementiDaRecuperare){
+        //         miglioritre.Add(p);
+
+        //         processati++;
+        //     }
+        // }
+
+        // return miglioritre;
+
+         // Sol 3
+         return _listaPunteggi.OrderByDescending(p=> p).Take(3).ToList();
+
+        // throw new NotImplementedException();
     }
 }
